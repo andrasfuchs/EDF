2fc06dc5687745aa52f09b10e68c0d78b6b9163b
Lior Banai <liorbanai@gmail.com>
2023-02-02 20:32:27 +0200

    fix parsing?

--- EDF/TAL.cs
+++ EDF/TAL.cs
@@ -235,12 +235,15 @@ return 0;
         }
         public static byte[] GetBytesForTALIndex(int index)
         {
+            var strIndex = index.ToString();
+            var leftSide = (strIndex.Length > 1) ? strIndex.Substring(0, strIndex.Length - 1) : "0";
+            var rightSide = strIndex.Substring(strIndex.Length - 1);
+
             List<byte> result = new List<byte>();
-            var leftSide = index / 10;
-            var rightSide = index % 10;
-            result.AddRange(Encoding.ASCII.GetBytes($"+{leftSide}"));
+            result.AddRange(Encoding.ASCII.GetBytes("+"));
+            result.AddRange(Encoding.ASCII.GetBytes(leftSide));
             result.Add(TAL.byte_46);
-            result.AddRange(Encoding.ASCII.GetBytes($"{rightSide}"));
+            result.AddRange(Encoding.ASCII.GetBytes(rightSide));
             result.Add(TAL.byte_20);
             result.Add(TAL.byte_20);
             result.Add(TAL.byte_0);
